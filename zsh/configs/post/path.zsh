7ce34cfc0e6a67fcc8bb92b3bff5b727162a2fe9
Todd Morningstar <todd@thoughtbot.com>
2018-12-17 23:02:38 -0800

    Comment out asdf and .git/safe :see_no_evil:

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -2,11 +2,11 @@
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
 # load ASDF, falling back to rbenv if not available
-if [ -d "$HOME/.asdf" ]; then
-  . $HOME/.asdf/asdf.sh
-fi
+#if [ -d "$HOME/.asdf" ]; then
+#  . $HOME/.asdf/asdf.sh
+#fi
 
 # mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
+#PATH=".git/safe/../../bin:$PATH"
 
 export -U PATH
